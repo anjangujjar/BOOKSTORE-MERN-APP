7cd63482ab1a61beac04d34ef0b80d962df5757c
ANJAN GUJJAR G V <144676055+anjangujjar@users.noreply.github.com>
2024-07-01 12:16:28 +0530

    This is a Bookstore app that is used for info of the books in the mongoDB database

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://anjan1310:root@cluster0.0tks7rq.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
